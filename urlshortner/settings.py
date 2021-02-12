44b5ad3f43089e7648303d98e7b94ca9824926ae
achintya-cell <achintyasingh.science@gmail.com>
2021-02-13 00:42:14 +0530

    Some more changes

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -76,15 +76,15 @@ WSGI_APPLICATION = 'urlshortner.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+# DATABASES = {
+#     'default': dj_database_url.config()
+# }
 
 
 # Password validation
